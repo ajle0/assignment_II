c34e5a08339136e68179d3cc726cba8b8aadab6c
Phillip Leo Jnr <phillip.leo@strathmore.edu>
2024-10-05 14:53:00 +0000

    change 15

--- templates/header.php
+++ templates/header.php
@@ -4,3 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/styles.css">
+    <title>User Registration</title>
+</head>
+<body>
