b0051621d5ff126d10a18b935d4588fbb439057d
Phillip Leo Jnr <phillip.leo@strathmore.edu>
2024-10-05 14:37:55 +0000

    change 2

--- config/db.php
+++ config/db.php
@@ -6,3 +6,9 @@ class Database {
     private $password = 'PLJunior@2003';
     public $conn;
     
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
