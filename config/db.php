120d68cd597ab8040ed1735081fff317a6a0e6cd
Phillip Leo Jnr <phillip.leo@strathmore.edu>
2024-10-05 14:38:55 +0000

    change 3

--- config/db.php
+++ config/db.php
@@ -11,4 +11,10 @@ class Database {
         try {
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
         }
+        return $this->conn;
+    }
+}
+?>
