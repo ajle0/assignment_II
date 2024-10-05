78062875cc2e4773ae62148a3a8b56008bf60632
Phillip Leo Jnr <phillip.leo@strathmore.edu>
2024-10-05 14:35:19 +0000

    change 1

---
+++ config/db.php
@@ -0,0 +1,8 @@
+<?php
+class Database {
+    private $host = 'root@127.0.0.1:3306';
+    private $db_name = 'user_db';
+    private $username = 'root';
+    private $password = 'PLJunior@2003';
+    public $conn;
+    
