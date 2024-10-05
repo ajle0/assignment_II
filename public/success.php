4d386d04196961cc8ebc9c2ff4a49be261b1d2f7
Phillip Leo Jnr <phillip.leo@strathmore.edu>
2024-10-05 15:11:05 +0000

    change 37

---
+++ public/success.php
@@ -0,0 +1,8 @@
+<?php include_once '../templates/header.php'; ?>
+
+<div class="container">
+    <h2>Success</h2>
+    <p>You have successfully registered or logged in.</p>
+</div>
+
+<?php include_once '../templates/footer.php'; ?>
