271077603ae7c538be450623a109dcd6944cdcfb
Phillip Leo Jnr <phillip.leo@strathmore.edu>
2024-10-05 14:50:26 +0000

    change 13

--- src/Authentication.php
+++ src/Authentication.php
@@ -3,3 +3,9 @@ class Authentication {
     public function generate2FACode() {
         return rand(100000, 999999);
     }
+
+    public function validate2FACode($inputCode, $userCode) {
+        return $inputCode === $userCode;
+    }
+}
+?>
