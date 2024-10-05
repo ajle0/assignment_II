9e12dfb98b155615f739a567a43cdecad052e8b8
Phillip Leo Jnr <phillip.leo@strathmore.edu>
2024-10-05 15:01:37 +0000

    change 23

--- public/index.php
+++ public/index.php
@@ -11,3 +11,7 @@
             <label for="email">Email</label>
             <input type="email" name="email" class="form-control" required>
         </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" name="password" class="form-control" required>
+        </div>
